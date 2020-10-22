dffc0b21b2a335fa8fdb1aad5135ea8937a07e5b
himanshu324 <51742128+himanshu324@users.noreply.github.com>
2020-10-22 15:41:40 +0530

    Add files via upload

---
+++ DFS.py
@@ -0,0 +1,58 @@
+from collections import defaultdict 
+  
+# This class represents a directed graph using 
+# adjacency list representation 
+class Graph: 
+  
+    # Constructor 
+    def __init__(self): 
+  
+        # default dictionary to store graph 
+        self.graph = defaultdict(list) 
+  
+    # function to add an edge to graph 
+    def addEdge(self, u, v): 
+        self.graph[u].append(v) 
+  
+    # A function used by DFS 
+    def DFSUtil(self, v, visited): 
+  
+        # Mark the current node as visited  
+        # and print it 
+        visited[v] = True
+        print(v, end = ' ') 
+  
+        # Recur for all the vertices  
+        # adjacent to this vertex 
+        for i in self.graph[v]: 
+            if visited[i] == False: 
+                self.DFSUtil(i, visited) 
+  
+    # The function to do DFS traversal. It uses 
+    # recursive DFSUtil() 
+    def DFS(self, v): 
+  
+        # Mark all the vertices as not visited 
+        visited = [False] * (max(self.graph)+1) 
+  
+        # Call the recursive helper function  
+        # to print DFS traversal 
+        self.DFSUtil(v, visited) 
+  
+# Driver code 
+  
+# Create a graph given  
+# in the above diagram 
+g = Graph() 
+while True:
+    print("Enter the edge-0 0 to exit:")
+    a,b=input().split()
+    if((a=='0') and (b=='0')):
+        break
+    else:
+        g.addEdge(a,b)
+
+
+  
+print("Following is DFS ") 
+g.DFS(a) 
